5fd9ecc69090c3e23498c329652a0f75b7e4bb37
astragenius <darthmalak86@gmail.com>
2021-06-10 20:10:41 +0200

    Added playerChoice function with prompt for input and toLowercase function to make it Case sensitive

--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 
 // function playerChoice with promnt function 
 
+function playerChoice() {
+    
+    let choice = prompt("Make a decision: ")
+   
+    return choice.toLowerCase(); // make player decision case sensitive (RoCK, paper, SCISSOR).
+}
+
+
+console.log(playerChoice());
