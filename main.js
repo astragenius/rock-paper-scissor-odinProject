b12d08755a364893160bc502bccabd542023e970
astragenius <darthmalak86@gmail.com>
2021-06-12 13:56:37 +0200

    add function playRound

--- main.js
+++ main.js
@@ -37,4 +37,61 @@ function random(max) {
     return Math.floor(Math.random() * max);
 }
 
-computerChoice();
+
+
+function playRound(playerChoice, computerChoice) { // function get player decision and computer decision
+
+        
+        // if conditional with && operator decides the winner or loser of the round.
+
+        if (playerChoice == "paper" && computerChoice == "rock") {
+
+           return alert("Spieler gewinnt mit Papier gegen Stein");
+
+
+        } else if (playerChoice == "paper" && computerChoice =="scissor") {
+
+            return alert("Spieler verliert mit Papier Schere");
+
+        } else if (playerChoice == "rock" && computerChoice == "paper") {
+
+            return alert("Spieler verliert mit Stein gegen Papier"); 
+
+        } else if (playerChoice == "rock" && computerChoice == "scissor") {
+
+            return alert("Spieler gewinnt mit Stein gegen Schere");
+
+        }else if (playerChoice == "scissor" && computerChoice == "paper") {
+
+            return alert("Spieler gewinnt mit Schere gegen Papier");
+
+        }else if (playerChoice == "scissor" && computerChoice =="rock") {
+
+            return alert("Spieler verliert mit Schere gegen Stein");
+
+        }else if (playerChoice == computerChoice) {
+
+            return alert("Unentschieden");
+        } else {
+
+            return alert("Keine auswahl getroffen");
+        }
+         
+}
+
+let playerChoice1 = playerChoice();
+let computerChoice2 = computerChoice();
+console.log(computerChoice2);
+playRound(playerChoice1, computerChoice2);
+
+
+
+
+
+
+
+
+
+
+
+
